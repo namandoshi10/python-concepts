184c46315ae285f03e084120066d5fcd17f90757
Naman <naman@saubhagyam.org>
2024-01-30 16:25:29 +0530

    update files on local

---
+++ oops/oops_exersice_app/08_employee_management.py
@@ -0,0 +1,8 @@
+'''
+w.a.p.p with oops concepts add employee with details.(id,name,age,bod,mobile,degi,salary)
+id is auto generate
+show employee using id
+remove employee using id
+exit
+'''
+
--- string_exercise/exercise_set1/q1.py
+++ string_exercise/exercise_set1/q1.py
@@ -5,5 +5,20 @@ if w.lower() in str_value.lower():
     reversed_w = w[::-1]
     result_str = str_value.replace(w, reversed_w)
     print(result_str)
+    # print(str_value.replace(w,w[::-1]))
 else:
     print("No match found.")
+
+
+
+# string="hello hoe are you"
+# a="hello"
+
+# def functon(a,x):
+#     if a in x:
+#         print("yes")
+#         print(string.replace("hello",a[::-1]))
+#     else:
+#         print("no")
+
+# functon(a,string)
